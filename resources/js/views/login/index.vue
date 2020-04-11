59e60f313f4aff97f1c4e45f2c04e04125837777
Tuan Duong <bacduong@gmail.com>
2020-04-11 10:58:09 +0700

    Laravel Sanctum standardizing - Remove token - Code cleanup

--- resources/js/views/login/index.vue
+++ resources/js/views/login/index.vue
@@ -96,14 +96,16 @@ export default {
       this.$refs.loginForm.validate(valid => {
         if (valid) {
           this.loading = true;
-          this.$store.dispatch('user/login', this.loginForm)
-            .then(() => {
-              this.$router.push({ path: this.redirect || '/' });
-              this.loading = false;
-            })
-            .catch(() => {
-              this.loading = false;
-            });
+          window.axios.get('/sanctum/csrf-cookie').then(() => {
+            this.$store.dispatch('user/login', this.loginForm)
+              .then(() => {
+                this.$router.push({ path: this.redirect || '/' });
+                this.loading = false;
+              })
+              .catch(() => {
+                this.loading = false;
+              });
+          });
         } else {
           console.log('error submit!!');
           return false;
